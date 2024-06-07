16a3687c31fd380e8839d55407aafb2f3b6b60c8
purstudent <politykoilya42@gmail.com>
2024-06-07 05:27:45 +0300

    Добавлен вывод общей информации о задании

--- POMOGITE/main.cpp
+++ POMOGITE/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #4. GIT\n";
+    cout << "Variant #5. Ethernet protocole\n";
+    cout << "Author: Ilya Politykoo\n";
     return 0;
 }
